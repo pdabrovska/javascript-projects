f7665ecb58ce9605446ca267ecb515cac4eef764
Paulina Dąbrowska <124510750+pdabrovska@users.noreply.github.com>
2023-07-26 18:19:04 +0200

    Add files via upload

---
+++ rock-paper-scissors-game/scripts/rock-paper-scissors.js
@@ -0,0 +1,154 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+
+updateScoreElement();
+
+/*if (score === null) { //score === null można zastąpić !score
+score = {
+  wins: 0,
+  losses: 0,
+  ties: 0
+};
+}*/
+
+let isAutoPlaying = false;
+let intervalID;
+function autoPlay(){
+  if(!isAutoPlaying) {
+    intervalID = setInterval(() => {
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+    document.querySelector('.auto-play-button').innerHTML = 'Stop Play'
+  } else {
+    clearInterval(intervalID);
+    isAutoPlaying = false;
+    document.querySelector('.auto-play-button').innerHTML = 'Auto Play'
+  }
+}
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p'){
+    playGame('paper');
+  } else if (event.key === 's'){
+    playGame('scissors');
+  } else if (event.key === 'a'){
+    autoPlay();
+  } else if (event.key === 'Backspace'){
+    resetScore();
+  }
+});
+
+document.querySelector('.js-auto-play-button').addEventListener('click', () => {
+  autoPlay();
+});
+
+document.querySelector('.js-reset-button').addEventListener('click', () => {
+  resetScore();
+});
+
+function resetScore(){
+  document.querySelector('.js-reset-score-message').innerHTML = `<p class="reset-message">Are you sure you want to reset the score? 
+    <button class="js-yes-button yes-button">Yes</button>
+    <button class="js-no-button no-button">No</button>`
+  
+  document.querySelector('.js-yes-button').addEventListener('click', () => {
+    score.wins = 0;
+    score.losses = 0;
+    score.ties = 0;
+    localStorage.removeItem('score');
+    updateScoreElement();
+    document.querySelector('.js-reset-score-message').innerHTML ='';
+  });
+
+  document.querySelector('.js-no-button').addEventListener('click', () => {
+    document.querySelector('.js-reset-score-message').innerHTML ='';
+  });
+}
+
+function playGame(playerMove) {
+const computerMove = pickComputerMove();
+let result = '';
+
+if (playerMove === 'scissors'){
+  if (computerMove === 'rock'){
+    result = 'You lose.';
+  } else if ( computerMove === 'paper'){
+    result = 'You win.';
+  } else if (computerMove === 'scissors'){
+    result = 'Tie.';
+  }
+  
+} else if (playerMove === 'paper') {
+  if (computerMove === 'rock') {
+    result = 'You win.';
+  } else if (computerMove === 'paper') {
+    result = 'Tie.';
+  } else if (computerMove === 'scissors') {
+    result = 'You lose.';
+  }
+  
+} else {
+  if (computerMove === 'rock'){
+    result = 'Tie.';
+  } else if ( computerMove === 'paper'){
+    result = 'You lose.';
+  } else if (computerMove === 'scissors'){
+    result = 'You win.';
+  }
+}
+
+if (result === 'You win.') {
+  score.wins += 1;
+} else if (result === 'You lose.') {
+  score.losses += 1;
+} else if (result === 'Tie.') {
+  score.ties += 1;
+}
+
+localStorage.setItem('score', JSON.stringify(score)); //local storage only supports Strings, json.stingify konwertuje score na stringa
+updateScoreElement();
+
+document.querySelector('.js-result').innerHTML = result;
+document.querySelector('.js-moves').innerHTML = `You
+  <img class="emoji" src="images/${playerMove}-emoji.png">
+  <img class="emoji" src="images/${computerMove}-emoji.png">
+Computer`;
+}
+
+function updateScoreElement(){
+document.querySelector('.js-score').innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`
+}
+
+function pickComputerMove() {
+const randomNumber = Math.random();
+let computerMove = '';
+
+if (randomNumber >= 0 && randomNumber < 1 / 3) {
+  computerMove = 'rock';
+} else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+  computerMove = 'paper';
+} else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+  computerMove = 'scissors';
+}
+
+return computerMove;
+}
